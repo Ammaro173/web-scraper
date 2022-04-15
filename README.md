d8cd9dfb2aa7d37ddc0ede40d0fd7d66b4c61f78
Ammaro173 <ammaraf97@gmail.com>
2022-04-16 00:39:53 +0300

    web-scraper-V1

---
+++ README.md
@@ -0,0 +1,19 @@
+# web Scarping
+
+**Author**: Ammar Abul-Feilat
+
+**Version**: 0.1.0
+
+## Overview
+
+A simple web scraper that can be used to extract data from websites. trialed url is "https://en.wikipedia.org/wiki/History_of_Mexico" finds the citiations of the page. And the count of the citiations.
+
+![image](./web_scraper_terminal.jpg)
+
+## Architecture
+
+This app is written using Python 3.9.5, following best practices.
+
+## Change Log
+
+version 0.1.0 ; 15/4/2022 Initiated
---
+++ web_scraper/web_scraper.py
@@ -0,0 +1,48 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/History_of_Mexico"
+# res = requests.get(url)
+
+
+def get_citations_needed_count(url):
+    """_summary_
+
+    Args:
+        url (_type_): takes a url as an argument
+
+    Returns:
+        _type_: returns the number of citations needed
+    """
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    result = soup.findAll("span", text="citation needed")
+    print(result, end="\n")
+    return len(result)
+
+
+def get_citations_needed_report(url):
+    """_summary_
+    Args:
+        url (_type_): takes a url as an argument
+
+    Returns:
+        _type_: returns a report of the number of citations needed
+    """
+    report = ""
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    sup_finder = soup.find_all("sup")
+    # print(sup_finder)
+
+    for sup in sup_finder:
+        if sup.findChildren("span"):
+            # print(sup)
+            report += f"\n ==> {sup.previous_sibling}[citation needed] ."
+
+    return report
+
+
+print("\n", "The number of citations are = ", get_citations_needed_count(url), end="\n")
+print("\n", "The final report := ", get_citations_needed_report(url), end="\n")
